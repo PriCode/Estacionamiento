e5dea6d0f939b4535275e36b0b136d832fea46b2
Jonaxz <gjonaxz@gmail.com>
2014-12-12 18:44:35 -0500

    interface v2

--- miraflorespark/protected/views/administrador/index.php
+++ miraflorespark/protected/views/administrador/index.php
@@ -57,7 +57,7 @@
 
 <?php echo TbHtml::beginFormTb(TbHtml::FORM_LAYOUT_HORIZONTAL); ?>
     <fieldset>
-	    <legend>Tarifa Miembros</legend>
+	    <h2>Tarifa Miembros</h2>
 	    
 	    <?php echo TbHtml::label('Por Hora', 'text'); ?>
 	    <?php echo TbHtml::textField('text', '', array('placeholder' => '....')); ?>
@@ -67,7 +67,7 @@
 	    
 	    
 
-	    <legend>Tarifa No Miembros</legend>
+	    <h2>Tarifa No Miembros</h2>
 
 
 	    <?php echo TbHtml::label('Por Hora', 'text'); ?>
@@ -81,8 +81,17 @@
 
 <legend>Lotes</legend>
 
+	<h2>Para Miembros</h2>
+
+	<h2>Para no Miembros</h2>
+
 
 <legend>Reporte</legend>
 
+	<h2>Mes </h2>
+		<?php echo TbHtml::label('Desde:', 'text'); ?>
+	    <?php echo TbHtml::textField('text', '', array('placeholder' => '....')); ?>
 
+	    <?php echo TbHtml::label('Hasta:', 'text'); ?>
+	    <?php echo TbHtml::textField('text', '', array('placeholder' => '....')); ?>
 
