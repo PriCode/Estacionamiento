297e6733e96b1ebb316c5f7af5cb6c4ab538d845
NhalimYuri <n@gmail.com>
2014-12-18 17:52:50 -0500

    secondtarifas

--- miraflorespark/protected/views/administrador/index.php
+++ miraflorespark/protected/views/administrador/index.php
@@ -3,7 +3,7 @@
 	    array('label' => 'Panel de Control'),	    
 	    array('label' => 'Contratos', 'url' => '?r=administrador/index', 'active' => true),
 	    array('label' => 'Clientes', 'url' => '?r=cliente/admin'),
-	    array('label' => 'Tarifa', 'url' => '?r=administrador/gtarifas'),    
+	    array('label' => 'Tarifa', 'url' => '?r=administrador/tarifa'),
 	    array('label' => 'Lotes', 'url' => '?r=administrador/glotes'),
 	    array('label' => 'Reportes', 'url' => '?r=administrador/greportes'),
 	    TbHtml::menuDivider(),
